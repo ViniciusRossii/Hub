dd6f28c0963a653db3aa391f9ec1cfe78d466afc
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2020-07-16 23:35:55 -0300

    Syntax Update

--- Hub/src/index.js
+++ Hub/src/index.js
@@ -1,77 +1,69 @@
+// Variáveis
+
+var menuNavItem
+const menuNavLinks = []
+const backgrounds = {
+    "bg0": "linear-gradient( 90.1deg, #2196f3 16.8%, rgba(0, 50, 255, 1) 105.2%)",
+    "bg1": "radial-gradient( circle farthest-corner at 10% 20%,  rgba(90,92,106,1) 0%, rgba(32,45,58,1) 81.3% )",
+    "bg2": "linear-gradient( 106.4deg,  rgba(255,104,192,1) 11.1%, rgba(104,84,249,1) 81.3% )",
+    "bg3": "linear-gradient( 179.2deg,  rgba(34,34,34,1) 0%, rgba(8,0,153,1) 29.7%, rgba(118,6,166,1) 63.4%, rgba(233,0,64,0.58) 100.1% )",
+    "bg4": "linear-gradient( 111.7deg,  rgba(251,198,6,1) 2.4%, rgba(224,82,95,1) 28.3%, rgba(194,78,154,1) 46.2%, rgba(32,173,190,1) 79.4%, rgba(22,158,95,1) 100.2% )"
+}
+
 // Função para definir o background
 
 function setBg() {
-    if (localStorage.getItem("hub-background") == 0 || localStorage.getItem("hub-background") == null) {
-        document.body.style.background = "linear-gradient( 90.1deg, #2196f3 16.8%, rgba(0, 50, 255, 1) 105.2%)"
-    }
-    if (localStorage.getItem("hub-background") == 1) {
-        document.body.style.background = "radial-gradient( circle farthest-corner at 10% 20%,  rgba(90,92,106,1) 0%, rgba(32,45,58,1) 81.3% )"
-    }
-    if (localStorage.getItem("hub-background") == 2) {
-        document.body.style.background = "linear-gradient( 106.4deg,  rgba(255,104,192,1) 11.1%, rgba(104,84,249,1) 81.3% )"
-    }
-    if (localStorage.getItem("hub-background") == 3) {
-        document.body.style.background = "linear-gradient( 179.2deg,  rgba(34,34,34,1) 0%, rgba(8,0,153,1) 29.7%, rgba(118,6,166,1) 63.4%, rgba(233,0,64,0.58) 100.1% )"
-    }
-    if (localStorage.getItem("hub-background") == 4) {
-        document.body.style.background = "linear-gradient( 111.7deg,  rgba(251,198,6,1) 2.4%, rgba(224,82,95,1) 28.3%, rgba(194,78,154,1) 46.2%, rgba(32,173,190,1) 79.4%, rgba(22,158,95,1) 100.2% )"
+    for (i = 0; i < document.getElementsByClassName("color-options").length; i++) {
+        if (localStorage.getItem("hub-background") == null) {
+            document.body.style.background = backgrounds.bg0
+            break
+        }
+        if (localStorage.getItem("hub-background") == i) {
+            document.body.style.background = backgrounds[`bg${i}`]
+        }
     }
 }
 
 // Configuração do Hamburger Menu
 
-document.getElementById("menu-color").addEventListener("click", () => {
+document.getElementById("menu-toggle").addEventListener("click", () => {
     document.getElementById("menu-color").classList.toggle("on")
 })
 
 // Configuração do Menu Color
 
-document.getElementById("color1").addEventListener("click", () => {
-    localStorage.setItem("hub-background", 0)
-    setBg()
-})
-
-document.getElementById("color2").addEventListener("click", () => {
-    localStorage.setItem("hub-background", 1)
-    setBg()
-})
-
-document.getElementById("color3").addEventListener("click", () => {
-    localStorage.setItem("hub-background", 2)
-    setBg()
-})
-
-document.getElementById("color4").addEventListener("click", () => {
-    localStorage.setItem("hub-background", 3)
-    setBg()
-})
-
-document.getElementById("color5").addEventListener("click", () => {
-    localStorage.setItem("hub-background", 4)
-    setBg()
-})
+for (i = 0; i < document.getElementsByClassName("color-options").length; i++) {
+    document.getElementsByClassName("color-options")[i].addEventListener("click", (event) => {
+        localStorage.setItem("hub-background", event.target.id.slice(5) - 1)
+        setBg()
+    })
+}
 
 // Configuração dos Menu de Navegação
 
-document.getElementById("url1").addEventListener("click", () => {
-    window.location.href = "https://youtube.com"
-})
+for (i = 0; i < document.getElementsByClassName("nav-options").length; i++) {
+    menuNavItem = document.getElementsByClassName("nav-options")[i].dataset.menuNavItem
+    if (menuNavItem == "music") { // Apenas para o Youtube Music
+        menuNavLinks.push(`https://${menuNavItem}.youtube.com`)
+        document.getElementsByClassName("nav-options")[i].addEventListener("click", (event) => {
+            window.location.href = `${menuNavLinks[event.target.id.slice(3) - 1]}`
+        })
+        continue
+    }
+    menuNavLinks.push(`https://${menuNavItem}.com`)
+    document.getElementsByClassName("nav-options")[i].addEventListener("click", (event) => {
+        window.location.href = `${menuNavLinks[event.target.id.slice(3) - 1]}`
+    })
+}
 
-document.getElementById("url2").addEventListener("click", () => {
-    window.location.href = "https://music.youtube.com"
-})
+// Display de Images do Menu de Navegação
 
-document.getElementById("url3").addEventListener("click", () => {
-    window.location.href = "https://instagram.com"
-})
+// Tamanho do ícone da URL5 editado no menu-nav.css
 
-document.getElementById("url4").addEventListener("click", () => {
-    window.location.href = "https://twitter.com"
-})
-
-document.getElementById("url5").addEventListener("click", () => {
-    window.location.href = "https://github.com"
-})
+for (i = 0; i < document.getElementsByClassName("nav-options").length; i++) {
+    menuNavItem = document.getElementsByClassName("nav-options")[i].dataset.menuNavItem
+    document.getElementsByClassName("nav-options")[i].style.backgroundImage = `url('icons/${menuNavItem}.png')`
+}
 
 // Configuração dos Títulos
 
